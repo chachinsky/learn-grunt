23c7fd659be0364ad98c1c5df10bdc1879bf14c5
Jose Medina <josemedina921@gmail.com>
2015-02-28 10:47:45 -0500

    init commit for connect a server

--- gruntfile.js
+++ gruntfile.js
@@ -3,11 +3,16 @@ module.exports = function(grunt){
 	grunt.initConfig({
 		jshint: {
 			all: ['Gruntfile.js', 'app/scripts/**/*.js']
+		},
+		connect: {
+			server: {				
+			}
 		}
 
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-connect');
 
 	grunt.registerTask('default', ['jshint']);
 };
