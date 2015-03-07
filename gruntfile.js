231e0874d0854a12731e8d7bfa763c951c3b3d75
Jose Medina <josemedina921@gmail.com>
2015-03-07 15:02:42 -0500

    initial commit for adding grunt-watch

--- gruntfile.js
+++ gruntfile.js
@@ -13,17 +13,24 @@ module.exports = function(grunt){
 				options:{
 					port: 9000,
 					base: 'app',
-					keepalive: true,
+					keepalive: false,
 					open: {
 						target: 'http://localhost:9000'
 					}
 				}		
+			}
+		},
+		watch: {
+			lintingtasks: {
+				files: ['app/scripts/**/*.js'],
+				tasks: ['jshint']
 			}
 		}
 
 	});
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['jshint', 'connect']);
+	grunt.registerTask('default', ['jshint', 'connect', 'watch']);
 };
